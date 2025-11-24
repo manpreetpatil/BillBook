dce44a47fc84aee791100d65cbc4b2ee3452eb9a
Manpreet <manpreet.patil@gmail.com>
2025-11-24 14:49:23 +0530

    no message

--- application/config/firebase.php
+++ application/config/firebase.php
@@ -12,10 +12,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $config['firebase'] = [
-    'apiKey' => "",
-    'authDomain' => "",
-    'projectId' => "",
-    'storageBucket' => "",
-    'messagingSenderId' => "",
-    'appId' => ""
+    'apiKey' => "AIzaSyAjm9pmibSkJrYXL8JAvM_zsgGvk8jFAxw",
+    'authDomain' => "billbook-b8cbd.firebaseapp.com",
+    'projectId' => "billbook-b8cbd",
+    'storageBucket' => "billbook-b8cbd.firebasestorage.app",
+    'messagingSenderId' => "958217538476",
+    'appId' => "1:958217538476:web:7ff9384b3222c4a9a58809"
 ];
